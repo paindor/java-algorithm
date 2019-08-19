edf155587dc3f98ab857f16dfbb8e7df07d876bf
최광진 <53929412+paindor@users.noreply.github.com>
2019-08-20 01:44:29 +0900

    Add files via upload

---
+++ study04/Student.java
@@ -0,0 +1,19 @@
+package study04;
+
+public class Student {
+	public String getBmi(String name, double height, double weight) {
+		
+		
+		String result ="";
+		
+		return result;
+		
+	}
+	
+	public String unknown(String name, String adress, int age, int balance, int belif) {
+		
+		return "1억대출가능";
+		
+	}
+
+}
