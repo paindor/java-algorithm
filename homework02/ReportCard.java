bcc1b7ef74ca7c676a9607e5b430df35207adcbe
paindor <paindorrr@gmail.com>
2019-08-20 20:20:36 +0900

    why

--- homework02/ReportCard.java
+++ homework02/ReportCard.java
@@ -1,43 +1,4 @@
-<<<<<<< HEAD
-package homework02;
 
-import java.util.Scanner;
-
-public class ReportCard {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub\
-		Scanner scan = new Scanner(System.in);
-		int[] scores = new int[5];
-		System.out.println("이름입력>");
-		String name = scan.next();
-		System.out.println("점수입력(국어, 영어, 수학)");
-		for(int i = 0 ;i < 3; i++) {
-			scores[i] = scan.nextInt();
-			
-		}
-		for(int i = 0 ; i< 3; i++) {
-			scores[3]+= scores[i];
-			
-		}
-		scores[4] = scores[3]/ 3;
-		
-		
-		
-		
-		System.out.println("이름    국어     영어    수학    총점    평균    합격여부");
-		System.out.print(name);
-		for(int i = 0;i < 5; i++) {
-			System.out.printf("%d   %d   %d  %d  %d");
-			
-		}
-		System.out.println();
-
-		
-	}
-
-}
-=======
 package homework02;
 /*성적표 프로그램 을 하나 개발해야 합니다.
 교수는 학생과 국어, 영어, 수학점수만 입력하면
@@ -98,4 +59,3 @@ public class ReportCard {
 	}
 
 }
->>>>>>> edf155587dc3f98ab857f16dfbb8e7df07d876bf
